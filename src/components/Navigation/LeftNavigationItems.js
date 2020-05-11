ce36bf16bb0c4fc1ca2e08ac85777ddaa2adf854
Filippos Siokouros <filippos.siokouros@pokerload.com>
2020-05-11 11:22:42 +0000

    Merged in PW-5_about-us-page (pull request #48)
    PW-5 about us page
    
    Approved-by: Cleanthis Evangelos Metaxas <cleanthis.metaxas@pokerload.com>

--- src/components/Navigation/LeftNavigationItems.js
+++ src/components/Navigation/LeftNavigationItems.js
@@ -5,6 +5,7 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import HomeIcon from '@material-ui/icons/Home';
 import BusinessIcon from '@material-ui/icons/Business';
+import PeopleIcon from '@material-ui/icons/People';
 import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
@@ -53,18 +54,18 @@ const LeftNavigationItems = () => {
 						<ListItemText primary="Business" />
 					</ListItem>
 				</a>
+				<a href="about-us" className={classes.a}>
+					<ListItem button key="About Us">
+						<ListItemIcon><PeopleIcon /></ListItemIcon>
+						<ListItemText primary="About Us" />
+					</ListItem>
+				</a>
 				<a href="faq" className={classes.a}>
 					<ListItem button key="FAQ">
 						<ListItemIcon><QuestionAnswerIcon /></ListItemIcon>
 						<ListItemText primary="FAQ" />
 					</ListItem>
 				</a>
-				{/* <a href="about-us" className={classes.a}>
-					<ListItem button key="About Us">
-						<ListItemIcon><PeopleIcon /></ListItemIcon>
-						<ListItemText primary="About Us" />
-					</ListItem>
-				</a> */}
 			</List>
 		</div>
 	);
